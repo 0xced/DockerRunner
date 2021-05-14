4d9127648308016139730e58c72c14f43768a370
Cédric Luthi <cedric.luthi@gmail.com>
2021-05-15 00:58:20 +0200

    Add MySql test with MySqlConnector

---
+++ tests/MySqlConnectorConfiguration.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using MySqlConnector;
+
+namespace DockerRunner.Tests
+{
+    public class MySqlConnectorConfiguration : DockerDatabaseContainerConfiguration
+    {
+        protected virtual string Database => "database";
+        protected virtual string User => "root";
+        protected virtual string Password => "docker";
+
+        /// <inheritdoc />
+        public override string ConnectionString(string host, ushort port)
+        {
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = host,
+                Port = port,
+                Database = Database,
+                UserID = User,
+                Password = Password,
+            };
+            return builder.ConnectionString;
+        }
+
+        /// <inheritdoc />
+        public override DbProviderFactory ProviderFactory => MySqlConnectorFactory.Instance;
+
+        /// <inheritdoc />
+        public override ushort? Port => 3306;
+
+        /// <inheritdoc />
+        public override string ImageName => "mysql/mysql-server";
+
+        /// <inheritdoc />
+        public override IReadOnlyDictionary<string, string> EnvironmentVariables => new Dictionary<string, string>
+        {
+            ["MYSQL_DATABASE"] = Database,
+            ["MYSQL_ROOT_PASSWORD"] = Password,
+            ["MYSQL_ROOT_HOST"] = "%",
+        };
+
+        public override TimeSpan Timeout => TimeSpan.FromMinutes(1);
+    }
+}
