bde859e77f96513373f614debbd39e2e1c2932d2
Cédric Luthi <cedric.luthi@gmail.com>
2021-06-10 20:20:50 +0200

    Skip MySQL Server 5.7 (MySqlConnector) test when running on GitHub Actions

--- tests/DockerDatabaseContainerRunnerTest.cs
+++ tests/DockerDatabaseContainerRunnerTest.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Threading.Tasks;
-using DockerRunner.Database.Oracle;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,12 +15,7 @@ namespace DockerRunner.Tests
         public async Task StartDockerDatabaseContainer()
         {
             // Arrange
-            var configuration = new TConfiguration();
-
-            // Oracle tests fail in many ways when running GitHub Actions
-            // * OracleException ORA-01882: timezone region not found
-            // * Running the command `docker pull gvenzl/oracle-xe:full` failed with exit code 1 and produced this error on stderr: failed to register layer: ApplyLayer exit status 1 stdout:  stderr: write /opt/oracle/product/18c/dbhomeXE/lib/libmkl_intel_ilp64.so: no space left on device
-            Skip.If(IsRunningOnGitHubActions() && configuration is OracleConfigurationBase, "Oracle test would fail when running on GitHub Actions.");
+            var configuration = CreateConfiguration();
 
             // Act
             await using var runner = await DockerDatabaseContainerRunner.StartAsync(configuration, RunningCommand, RanCommand, waitOnDispose: true);
@@ -38,6 +32,26 @@ namespace DockerRunner.Tests
             }
         }
 
-        private static bool IsRunningOnGitHubActions() => bool.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"), out var result) && result;
+        private static TConfiguration CreateConfiguration()
+        {
+            var configuration = new TConfiguration();
+
+            var isRunningOnGitHubActions = bool.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"), out var result) && result;
+
+            // Oracle tests fail in many ways when running GitHub Actions
+            // * OracleException ORA-01882: timezone region not found
+            // * Running the command `docker pull gvenzl/oracle-xe:full` failed with exit code 1 and produced this error on stderr: failed to register layer: ApplyLayer exit status 1 stdout:  stderr: write /opt/oracle/product/18c/dbhomeXE/lib/libmkl_intel_ilp64.so: no space left on device
+            Skip.If(isRunningOnGitHubActions && configuration is Database.Oracle.OracleConfigurationBase, "Oracle test would fail when running on GitHub Actions.");
+
+            // This consistently fails with the following exception:
+            // System.TimeoutException : Database was not available on "Server=127.0.0.1;Port=49211;Database=database;User Id=root;***" after waiting for 30.0 seconds.
+            // ---- MySqlConnector.MySqlException : SSL Authentication Error
+            // -------- System.Security.Authentication.AuthenticationException : Authentication failed, see inner exception.
+            // ------------ Interop+OpenSsl+SslException : SSL Handshake failed with OpenSSL error - SSL_ERROR_SSL.
+            // ---------------- Interop+Crypto+OpenSslCryptographicException : error:14094410:SSL routines:ssl3_read_bytes:sslv3 alert handshake failure
+            Skip.If(isRunningOnGitHubActions && configuration is Database.MySqlConnector.MySqlServer57Configuration, "MySQL Server 5.7 (MySqlConnector) test would fail when running on GitHub Actions.");
+
+            return configuration;
+        }
     }
 }
