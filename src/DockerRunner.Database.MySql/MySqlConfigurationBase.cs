4e224c8ae630c103793f76b55ad00cfce8406ec2
Cédric Luthi <cedric.luthi@gmail.com>
2021-06-10 19:05:58 +0200

    Increase MySql timeout to 1 minute

--- src/DockerRunner.Database.MySql/MySqlConfigurationBase.cs
+++ src/DockerRunner.Database.MySql/MySqlConfigurationBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.Common;
 
 #if DRIVER_MYSQLDATA
@@ -47,6 +48,9 @@ namespace DockerRunner.Database.MySqlConnector
 
         /// <inheritdoc />
         public override ushort? Port => 3306;
+
+        /// <inheritdoc />
+        public override TimeSpan Timeout => TimeSpan.FromMinutes(1);
 
         /// <inheritdoc />
         public override IReadOnlyDictionary<string, string> EnvironmentVariables => new Dictionary<string, string>
