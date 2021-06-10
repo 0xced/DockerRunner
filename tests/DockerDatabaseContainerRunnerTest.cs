066739026e1f46a4112a70214fb288644dc1528d
Cédric Luthi <cedric.luthi@gmail.com>
2021-06-10 17:32:10 +0200

    Skip Oracle tests when running on GitHub Actions
    They would fail with one of the following errors:
    
    * OracleException ORA-01882: timezone region not found
    * Running the command `docker pull gvenzl/oracle-xe:full` failed with exit code 1 and produced this error on stderr: failed to register layer: ApplyLayer exit status 1 stdout:  stderr: write /opt/oracle/product/18c/dbhomeXE/lib/libmkl_intel_ilp64.so: no space left on device

--- tests/DockerDatabaseContainerRunnerTest.cs
+++ tests/DockerDatabaseContainerRunnerTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Threading.Tasks;
+using DockerRunner.Database.Oracle;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,11 +12,16 @@ namespace DockerRunner.Tests
     {
         public DockerDatabaseContainerRunnerTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper) {}
 
-        [Fact]
+        [SkippableFact]
         public async Task StartDockerDatabaseContainer()
         {
             // Arrange
             var configuration = new TConfiguration();
+
+            // Oracle tests fail in many ways when running GitHub Actions
+            // * OracleException ORA-01882: timezone region not found
+            // * Running the command `docker pull gvenzl/oracle-xe:full` failed with exit code 1 and produced this error on stderr: failed to register layer: ApplyLayer exit status 1 stdout:  stderr: write /opt/oracle/product/18c/dbhomeXE/lib/libmkl_intel_ilp64.so: no space left on device
+            Skip.If(IsRunningOnGitHubActions() && configuration is OracleConfigurationBase, "Oracle test would fail when running on GitHub Actions.");
 
             // Act
             await using var runner = await DockerDatabaseContainerRunner.StartAsync(configuration, RunningCommand, RanCommand, waitOnDispose: true);
@@ -30,5 +37,7 @@ namespace DockerRunner.Tests
                 TestOutputHelper.WriteLine($"  PortMapping: {portMapping.HostEndpoint} -> {portMapping.ContainerPort}");
             }
         }
+
+        private static bool IsRunningOnGitHubActions() => bool.TryParse(Environment.GetEnvironmentVariable("GITHUB_ACTIONS"), out var result) && result;
     }
 }
